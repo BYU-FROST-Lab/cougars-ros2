f6d3cf988879ae6c472f653193f2722a8fd4713e
ClaytonSmith314 <claytonsmith3.14@gmail.com>
2026-03-04 19:30:15 -0700

    Setup modes.  Going to change to make simpler.

---
+++ cougars_bringup/launch/modes.py
@@ -0,0 +1,107 @@
+
+# ============================================== #
+#   enumerates which launch configurations are 
+#   associated with each mode in the graph.
+#   Typically, configurations specify which
+#   nodes are too be launch in a particular mode
+# ============================================== #
+
+
+MODES = {
+    "full" : {
+        "__inherits__": ['sim', '']
+    },
+    "sim"  : {},
+    "surface" : {},
+    "demo" : {},
+    "sensors" : {},
+
+    "core" : {},
+
+
+    "no_bridge": {
+        "depth_converter_on": False,
+        "dvl_converter_on": False,
+        "dvl_global_on": False,
+        "seatrac_ahrs_converter_on": False
+    }
+}
+
+COMPOUND_FLAGS = {
+
+}
+
+DEFAULTS = {
+
+}
+
+
+
+
+
+def resolve_modes(mode_list, flag_overrides):
+    """
+    mode_list: ["manual", "sim"]
+    flag_overrides: {"use_navigation": True, "use_gps": False}
+
+    Returns dict of launch configs to set.
+    """
+
+    resolved = dict(DEFAULTS)
+
+    # update a mode with mode inheritance
+    def update_mode(resolved:dict, mode:str, visited:set=None):
+        if mode not in MODES:
+            raise RuntimeError(f"Unknown mode: {mode}")
+        if visited is None: visited = {}
+        # update parents first so child mode overrides
+        for parent in MODES[mode]['__inherits__']:
+            if parent not in visited:
+                visited.add(parent)
+                update_mode(resolved, parent, visited)
+        resolved.update(MODES[mode])
+        del resolved['__inherits__'] # don't include special __inherits__ in final
+
+    # Apply modes from lowest → highest priority
+    # first mode wins → so apply reversed
+    for mode in reversed(mode_list):
+        update_mode(resolved, mode)
+
+    # Explicit flags override everything
+    resolved.update(flag_overrides)
+
+    return resolved
+
+
+def parse_list(value: str):
+    if not value:
+        return []
+    return [x.strip() for x in value.split(",") if x.strip()]
+
+
+def parse_flags(flag_str: str):
+    """
+    use_gps:false,use_navigation:true, verbosity_level:5
+    """
+    result = {}
+    if not flag_str:
+        return result
+
+    for item in flag_str.split(","):
+        item = item.strip()
+        if not item:
+            continue
+
+        if ":" in item:
+            k, v = item.split(":", 1)
+            if v.strip().lower() == "true":
+                result[k.strip()] = True
+            elif v.strip().lower() == "false":
+                result[k.strip()] = False
+            else: 
+                result[k.strip()] = v.strip()
+        else:
+            # shorthand → true
+            result[item] = True
+
+    return result
--- cougars_bringup/launch/persistant_launch.py
+++ cougars_bringup/launch/persistant_launch.py
@@ -1,16 +1,14 @@
-import sys
-
+import os
 import launch
 import launch_ros.actions
 from launch.actions import DeclareLaunchArgument
 import launch_ros.descriptions
 from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition
 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
-import os
+from modes import *
 
 # 
 # Top level launch file.
@@ -22,97 +20,125 @@ import os
 def generate_launch_description():
     
   ### Launch arguments
-    namespace_launch_arg = DeclareLaunchArgument(
+    namespace_arg = DeclareLaunchArgument(
         'namespace',
-        default_value='coug0')
+        default_value='coug0',
+        description='Namespace for the vehicle'
+    )
     
-    sim_launch_arg = DeclareLaunchArgument(
-        'sim',
-        default_value='False')
-    
-    param_file_launch_arg = DeclareLaunchArgument(
+    param_file_arg = DeclareLaunchArgument(
         'param_file',
-        default_value='/home/frostlab/config/deploy_tmp/vehicle_params.yaml')
+        default_value='/home/frostlab/config/deploy_tmp/vehicle_params.yaml',
+        description='Path to the vehicle parameter file'
+    )
     
-    fleet_param_launch_arg = DeclareLaunchArgument(
+    fleet_param_arg = DeclareLaunchArgument(
         'fleet_param',
-        default_value='/home/frostlab/config/deploy_tmp/fleet_params.yaml')
+        default_value='/home/frostlab/config/deploy_tmp/fleet_params.yaml',
+        description='Path to the fleet parameter file'
+    )
     
-    verbose_launch_arg = DeclareLaunchArgument(
-        'verbose',
-        default_value='False')
+    mode_arg = DeclareLaunchArgument(
+        'modes',
+        default_value='full',
+        description=
+        'Comma-separated list of system modes. ' \
+        'Each mode specifies a list of launch configurations to set for . ' \
+        'Modes are applied in order; ' \
+        'earlier modes take precedence when settings conflict.' \
+        'Existing modes include: full, sim, sensors, surface, demo'
+    )
+
+    flags_arg = DeclareLaunchArgument(
+        'flags',
+        default_value='',
+        description=
+        'Comma-separate list of launch argument overrides.' \
+        'Takes precidence over modes.'
+    )
     
 
-
-  ### Launch arguments list
-    launch_arguments = [
-        ('namespace', LaunchConfiguration('namespace')),
-        ('sim', LaunchConfiguration('sim')),
-        ('param_file', LaunchConfiguration('param_file')),
+  ### List of launch arguments to pass to branch launch files
+    launch_args = [
+        ('namespace',   LaunchConfiguration('namespace')),
+        ('param_file',  LaunchConfiguration('param_file')),
         ('fleet_param', LaunchConfiguration('fleet_param')),
-        ('verbose', LaunchConfiguration('verbose')),
+        ('modes',       LaunchConfiguration('modes')),
+        ('flags',       LaunchConfiguration('flags'))
     ]
 
+  ### Parse modes and flags to get the remaining launch arguments
+    flags = parse_flags(launch_args['flags'])
+    mode_list = parse_list(launch_args['modes'])
+    resolved_args = resolve_modes(mode_list, flags)
+    launch_args.extend(resolved_args.items())
 
   ### Package launch directories
-    cougars_bridge_dir = os.path.join(
+    bridge_dir = os.path.join(
         get_package_share_directory('cougars_bridge'), 'launch')
-    cougars_bringup_dir = os.path.join(
+    bringup_dir = os.path.join(
         get_package_share_directory('cougars_bringup'), 'launch')
-    cougars_control_dir = os.path.join(
+    control_dir = os.path.join(
         get_package_share_directory('cougars_control'), 'launch')
-    cougars_coms_dir = os.path.join(
+    coms_dir = os.path.join(
         get_package_share_directory('cougars_coms'), 'launch')
-    cougars_description_dir = os.path.join(
+    description_dir = os.path.join(
         get_package_share_directory('cougars_description'), 'launch')   
-    cougars_localization_dir = os.path.join(
+    localization_dir = os.path.join(
         get_package_share_directory('cougars_localization'), 'launch')
-
 
 
   ### Launch files
     bridge_launch = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(cougars_bridge_dir, "cougars_bridge_launch.py")),
-        launch_arguments=launch_arguments)
+        PythonLaunchDescriptionSource(
+            os.path.join(bridge_dir, "cougars_bridge_launch.py")),
+        launch_arguments=launch_args)
     
     coms_launch = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(cougars_coms_dir, "coms_launch.py")),
-        launch_arguments=launch_arguments)
+        PythonLaunchDescriptionSource(
+            os.path.join(coms_dir, "coms_launch.py")),
+        launch_arguments=launch_args)
     
     control_launch = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(cougars_control_dir, "control_launch.py")),
-        launch_arguments=launch_arguments)
+        PythonLaunchDescriptionSource(
+            os.path.join(control_dir, "control_launch.py")),
+        launch_arguments=launch_args)
     
     waypoint_launch = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(cougars_control_dir, "waypoint_launch.py")),
-        launch_arguments=launch_arguments)
+        PythonLaunchDescriptionSource(
+            os.path.join(control_dir, "waypoint_launch.py")),
+        launch_arguments=launch_args)
     
     description_launch = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(cougars_description_dir, "cougars_description_launch.py")),
-        launch_arguments=launch_arguments)
+        PythonLaunchDescriptionSource(
+            os.path.join(description_dir, "cougars_description_launch.py")),
+        launch_arguments=launch_args)
     
     localization_launch = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(cougars_localization_dir, "cougars_localization_launch.py")),
-        launch_arguments=launch_arguments)
+        PythonLaunchDescriptionSource(
+            os.path.join(localization_dir, "cougars_localization_launch.py")),
+        launch_arguments=launch_args)
     
     sensors_launch = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(cougars_bringup_dir, "sensors_launch.py")),
-        launch_arguments=launch_arguments)
+        PythonLaunchDescriptionSource(
+            os.path.join(bringup_dir, "sensors_launch.py")),
+        launch_arguments=launch_args)
     
     recorder_diagnostics_launch = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(cougars_bringup_dir, "recorder_diagnostics_launch.py")),
-        launch_arguments=launch_arguments)
+        PythonLaunchDescriptionSource(
+            os.path.join(bringup_dir, "recorder_diagnostics_launch.py")),
+        launch_arguments=launch_args)
     
     
 
   ### Launch actions
     launch_actions = [
         # launch arguments
-        namespace_launch_arg,
-        sim_launch_arg,
-        param_file_launch_arg,
-        fleet_param_launch_arg,
-        verbose_launch_arg,
+        namespace_arg,
+        param_file_arg,
+        fleet_param_arg,
+        mode_arg,
+        flags_arg,
 
         # launch files
         bridge_launch,
